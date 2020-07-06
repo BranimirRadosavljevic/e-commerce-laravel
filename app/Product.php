8d9b66d6c4c7cad0703edccb1aca5f8da94bd5e0
Bane Radosavljević <r_bane@hotmail.com>
2020-07-06 07:50:42 +0200

    Episode 2

--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,11 @@ class Product extends Model
 {
     public function presentPrice()
     {
-        return money_format('$%i', $this->price / 100);
+        return number_format($this->price, 2, ',', '.') . ' RSD';
     }
+
+    public function scopeMightAlsoLike($query)
+    {
+        return $query->inRandomOrder()->take(4);
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+
 Route::get('/', 'LandingPageController@index')->name('landing-page');
 
 Route::get('/shop', 'ShopController@index')->name('shop.index');
 Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 
-Route::view('/cart', 'cart');
+Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/cart', 'CartController@store')->name('cart.store');
+Route::delete('/cart/{product}', 'CartController@destroy')->name('cart.destroy');
+
+Route::get('empty', function(){
+    Cart::destroy();
+});
+
 Route::view('/checkout', 'checkout');
 Route::view('/thankyou', 'thankyou');
