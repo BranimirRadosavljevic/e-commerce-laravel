434959f3159d629db492546cf758c216b5876cf5
Branimir Radosavljevic <rbranimir@ymail.com>
2020-07-22 15:54:51 +0200

    Episode 21 complete

--- public/js/algolia.js
+++ public/js/algolia.js
@@ -6,15 +6,31 @@ const products = client.initIndex('products');
 autocomplete('#aa-search-input', {}, [
     {
       source: autocomplete.sources.hits(products, { hitsPerPage: 10 }),
-      displayKey: 'name',
+      displayKey: 'name', 
       templates: {
         header: '<div class="aa-suggestions-category">Products</div>',
-        suggestion({_highlightResult}) {
-          return `<span>${_highlightResult.name.value}</span>`;
+        suggestion: function (suggestion) {
+          const markup = `
+            <div class="algolia-result">              
+            <span>
+            ${suggestion._highlightResult.name.value}
+            </span>
+            <img src="${window.location.origin}/storage/${suggestion.image}" alt="img" class="algolia-thumb">
+            </div>
+            <div class="algolia-details">
+              <span>${suggestion._highlightResult.details.value}</span>
+            </div>
+          `;
+          return markup;
+        },
+        empty({result}){
+          return `<span class="aa-empty">No result.</span>`;
         }
       }
     },
     
-]);
+]).on('autocomplete:selected', function(event, suggestion, dataset){
+  window.location.href = window.location.origin + "/shop/" + suggestion.slug;
+});
 
 })();
